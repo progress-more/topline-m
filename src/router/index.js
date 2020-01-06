31e562c3cb16830e354cef60fa24c5577072966e
progress-more <991414354@qq.com>
2020-01-06 20:05:10 +0800

    初始化预处理器文件结构

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  { path: '/login', component: () => '../views/login' }
   // route level code-splitting
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
