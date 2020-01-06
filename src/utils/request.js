4da804d339c86479d9451fb6ec7d49ed5fad0761
progress-more <991414354@qq.com>
2020-01-06 20:52:36 +0800

    处理后端返回数据超出js安全整数范围问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,10 +2,22 @@
 // 将每一个请求都封装成一个个函数 在需要时加载调用即可
 
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 
 // axios.create()复制一个axios
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
+request.defaults.transformResponse = [function (data) {
+  try {
+    return jsonBig.parse(data)
+  } catch (error) {
+    return {}
+  }
+}]
+
+// 请求拦截器
+
+// 响应拦截器
 
 export default request
