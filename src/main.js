f8340f353657730d6c3b22902d07e149344173a9
progress-more <991414354@qq.com>
2020-01-09 11:14:01 +0800

    登录表单验证初始配置

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import './utils/register-vant'
 import './styles/index.less'// 加载全局样式 要放在第三方样式之后
 import 'amfe-flexible'
+import './utils/validation' // 加载表单验证组件
 Vue.config.productionTip = false
 
 new Vue({
