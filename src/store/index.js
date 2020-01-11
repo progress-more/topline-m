33c1e7f747c190f55fd930c97b89740f04f42155
progress-more <991414354@qq.com>
2020-01-11 10:04:40 +0800

    持久化token存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,20 @@
 // vuex容器
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { setItem, getItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     // 设置登录用户信息 对象 存储token信息
-    user: null
+    user: getItem('user')
   },
   mutations: {
     setUser (state, data) {
       state.user = data
+      // 为了防止刷新丢失 state中的user状态 ，我们把它放到本地存储
+      setItem('user', state.user)
     }
   },
   actions: {
