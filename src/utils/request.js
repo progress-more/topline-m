0e3e4e2394b447812f1cf507c2b030a8aace0913
progress-more <991414354@qq.com>
2020-01-11 11:09:00 +0800

    设置响应拦截器-处理token过期

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,11 +4,13 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
 import store from '@/store'
+import router from '@/router'
 
 // axios.create()复制一个axios
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
+// 处理后端返回数据中超出js 安全整数范围的问题
 request.defaults.transformResponse = [function (data) {
   try {
     return jsonBig.parse(data)
@@ -36,5 +38,55 @@ request.interceptors.request.use(function (config) {
 })
 
 // 响应拦截器
+request.interceptors.response.use(
+  // 响应成功进入第一个函数
+  // 该函数的参数是响应对象
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  // 响应失败进入第二个函数 ，该函数的参数是错误对象
+  async function (error) {
+    // 如果响应码是401 则请求新的token
+    // 响应拦截器中的 error 就是相应的错误对象
+    if (error.response && error.response.status === 401) {
+      // 校验是否有refresh_token
+      const user = store.state.user
+      if (!user || !user.refresh_token) {
+        router.push('/login')
+        // 代码不往后执行
+        return
+      }
+
+      // 如果有refresh_token ，则请求获取新的token
+      try {
+        const res = await axios({
+          method: 'PUT',
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          headers: {
+            Authorization: `Bearer ${user.refresh_token}`
+          }
+        })
+
+        // 如果获取成功 则把新的token更新到容器中
+        // console.log('刷新token成功', res)
+        store.commit('setUser', {
+          token: res.data.data.token, // 最新获取的可用token
+          refresh_token: user.refresh_token // 还是原来的refresh_token
+        })
+
+        // 把之前失败的用户请求继续发出去
+        // config 是一个对象 其中包含本次失败请求相关的那些配置信息，
+        // return 把request 的请求结果继续返回给发请求的具体位置
+        return request(error.config)
+      } catch (error) {
+        // 如果获取失败，直接跳转登录页
+        router.push('/login')
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default request
