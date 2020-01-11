15aa1da336a819a23044e044933bdf528a0e2131
progress-more <991414354@qq.com>
2020-01-11 10:24:43 +0800

    请求拦截器-统一放置token

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,7 @@
 
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+import store from '@/store'
 
 // axios.create()复制一个axios
 const request = axios.create({
@@ -17,6 +18,22 @@ request.defaults.transformResponse = [function (data) {
 }]
 
 // 请求拦截器
+request.interceptors.request.use(function (config) {
+  // config 为请求配置对象 ，我们可以通过修改 config 来实现统一请求数据处理
+  const { user } = store.state
+
+  // 统一添加 token
+  if (user) {
+    // config.headers 获取操作请求头对象
+    // Authorization 是后端要求的字段名称
+    // 数据值后端要求提供： Bearer 空格 token数据
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // 请求错误时 返回promise的reject（）函数 进入异步请求axios的catch中
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 
