24b65bd4928d01f651eff75dc30783f4c00806d7
progress-more <991414354@qq.com>
2020-01-12 14:35:06 +0800

    首页-调整样式

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar title="首页"/>
+    <van-nav-bar title="首页" fixed/>
     <!-- 频道列表 -->
     <!-- tab组件 v-model=‘active’控制被激活的标签
     title 标签标题 -->
@@ -46,6 +46,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+// 父组件的组用于默认只能影响到子组件的根节点
+// 若要影响深 需用深度选择器 或不要作用域
+  .home-container {
+    padding-top: 90px;
+    padding-bottom: 50px;
+    /deep/ .van-tabs__wrap {
+      position: fixed;
+      top: 46px;
+      left: 0;
+      right: 0;
+      z-index: 2;
+    }
+  }
 </style>
