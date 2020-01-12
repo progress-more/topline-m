a8569c9e947a74001ac56dc14ef14961ec21568b
progress-more <991414354@qq.com>
2020-01-12 13:25:12 +0800

    加载文章列表

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- 文章列表组件 -->
+     <!-- 下拉刷新 -->
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <!-- 文章列表 -->
+        <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+          <van-cell
+            v-for="(article,index) in list"
+            :key="index"
+            :title="article.title"
+          />
+        </van-list>
+    </van-pull-refresh>
+</template>
+
+<script>
+import { getArticles } from '@/api/article'
+export default {
+  name: 'articleList',
+  props: {
+    channel: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      isLoading: false,
+      list: [],
+      loading: false,
+      finished: false,
+      timestamp: null // 用于获取下一页数据的时间戳
+    }
+  },
+  methods: {
+    // 下拉刷新
+    onRefresh () {
+      setTimeout(() => {
+        this.$toast('刷新成功')
+        this.isLoading = false
+      }, 1000)
+    },
+    // 上拉加载更多调用onload
+    async onLoad () {
+      // 1.请求获取数据
+      const { data } = await getArticles({
+        channel_id: this.channel.id, // 频道id
+        // 第一次使用 Date.now()获取最新数据
+        // 下一页的数据使用本次返回的数据中的timestamp
+        timestamp: this.timestamp || Date.now(), // 时间戳 ，
+        // 请求新的推荐数据传当前的时间戳 请求历史推荐传指定的时间戳
+        with_top: 1
+      })// 是否包含置顶，进入页面第一次请求时要包含置顶文章，1包含 0 不包含
+      //   2.把请求获取到的数据添加到数组列表中
+      const results = data.data.results
+      this.list.push(...results)
+      // 3.加载状态结束
+      this.loading = false
+      // 4.数据全部加载完成
+      if (results.length) {
+        //   更新获取下一页数据的时间戳
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        //   没有数据了，把finished设置为true
+        this.finished = true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,25 +7,12 @@
     title 标签标题 -->
     <van-tabs v-model="active">
       <van-tab
-      :title="item.name"
-      v-for="item in userChannels"
-      :key="item.id">
-        <!-- 下拉刷新 -->
-        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
-        <van-list
-            v-model="loading"
-            :finished="finished"
-            finished-text="没有更多了"
-            @load="onLoad"
-          >
-          <van-cell
-            v-for="item in list"
-            :key="item"
-            :title="item"
-          />
-        </van-list>
-        </van-pull-refresh>
-         <!-- 文章列表 -->
+      :title="channel.name"
+      v-for="channel in userChannels"
+      :key="channel.id">
+
+       <!-- 文章列表组件 -->
+       <article-list :channel='channel'/>
       </van-tab>
     </van-tabs>
   </div>
@@ -33,41 +20,20 @@
 
 <script>
 import { getUserChannels } from '@/api/user'
+// 引入文章列表组件
+import ArticleList from './components/article-list'
 export default {
   name: 'homePage',
+  components: {
+    ArticleList
+  },
   data () {
     return {
       active: 0,
-      isLoading: false,
-      list: [],
-      loading: false,
-      finished: false,
       userChannels: [] // 接收用户频道列表
     }
   },
   methods: {
-    // 下拉刷新
-    onRefresh () {
-      setTimeout(() => {
-        this.$toast('刷新成功')
-        this.isLoading = false
-      }, 1000)
-    },
-    // 上拉加载更多调用onload
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
-    },
     // 获取用户频道列表
     async loadUserChannels () {
       const { data } = await getUserChannels()
